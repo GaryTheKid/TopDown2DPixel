f46529400a394fc0b0ccccc88bd16b979004b8c1
GaryTheKid <55671706+GaryTheKid@users.noreply.github.com>
2023-05-24 22:55:03 -0400

    23.5.24 update the login/sign scene

--- Assets/Scripts/Backend/AuthManager.cs
+++ Assets/Scripts/Backend/AuthManager.cs
@@ -6,13 +6,37 @@ using UnityEngine.UI;
 public class AuthManager : MonoBehaviour
 {
     [Header("UI References")]
-    public InputField emailInputField;
-    public InputField passwordInputField;
+
+    // input fields
+    [Header("Input Fields")]
+    public InputField emailInputField_Login;
+    public InputField passwordInputField_Login;
+    public InputField emailInputField_SignUp;
+    public InputField passwordInputField_SignUp;
+    public InputField passwordDoubleCheckField;
+
+    // alert text
+    [Header("Alert Text")]
+    public Text emailInputAlertText_Login;
+    public Text passwordInputAlertText_Login;
+    public Text emailInputAlertText_SignUp;
+    public Text passwordInputAlertText_SignUp;
+
+    // buttons
+    [Header("Buttons")]
     public Button loginButton;
+    public Button createNewAccountButton;
     public Button signupButton;
+    public Button backToLoginButton;
     public Button signoutButton;
 
+    // login and signup pages
+    [Header("Pages")]
+    public GameObject loginPage;
+    public GameObject signUpPage;
+
     private FirebaseAuth auth;
+    private bool isEmailValid = true;
 
     private void Start()
     {
@@ -21,14 +45,96 @@ public class AuthManager : MonoBehaviour
 
         // Add event listeners to buttons
         loginButton.onClick.AddListener(Login);
+        createNewAccountButton.onClick.AddListener(GotoSignUpPage);
         signupButton.onClick.AddListener(Signup);
+        backToLoginButton.onClick.AddListener(GotoLoginPage);
         signoutButton.onClick.AddListener(SignOut);
+    }
+
+    private void Update()
+    {
+        // Check email validation each frame
+        ValidateEmail();
+    }
+
+    private void GotoSignUpPage()
+    {
+        loginPage.SetActive(false);
+        signUpPage.SetActive(true);
+    }
+
+    private void GotoLoginPage()
+    {
+        signUpPage.SetActive(false);
+        loginPage.SetActive(true);
+    }
+
+    private void ValidateEmail()
+    {
+        InputField activeInputField;
+        Text activeAlertText;
+
+        // Check the active page and assign the corresponding input field and alert text
+        if (loginPage.activeSelf)
+        {
+            activeInputField = emailInputField_Login;
+            activeAlertText = emailInputAlertText_Login;
+        }
+        else if (signUpPage.activeSelf)
+        {
+            activeInputField = emailInputField_SignUp;
+            activeAlertText = emailInputAlertText_SignUp;
+        }
+        else
+        {
+            return; // No active page, no need for validation
+        }
+
+        string email = activeInputField.text;
+
+        // Check if email input is empty
+        if (string.IsNullOrEmpty(email))
+        {
+            // Reset the input field's image color to its default
+            Color initColor = Color.white;
+            initColor.a = activeInputField.image.color.a; // Preserve input field's transparency
+            activeInputField.image.color = initColor;
+            activeAlertText.text = string.Empty;
+            return;
+        }
+
+        bool isValid = ValidateEmailFormat(email);
+
+        // Provide feedback to the user (you can modify this based on your UI requirements)
+        Color color = isValid ? Color.white : Color.red;
+        color.a = activeInputField.image.color.a; // Preserve input field's transparency
+        activeInputField.image.color = color;
+
+        activeAlertText.text = isValid ? string.Empty : "Invalid email format";
+    }
+
+    private bool ValidateEmailFormat(string email)
+    {
+        if (!email.Contains("@") || !email.Contains("."))
+            return false;
+
+        int atIndex = email.IndexOf("@");
+        int dotIndex = email.LastIndexOf(".");
+
+        if (atIndex >= dotIndex)
+            return false;
+
+        string username = email.Substring(0, atIndex);
+        string domain = email.Substring(atIndex + 1, dotIndex - atIndex - 1);
+        string extension = email.Substring(dotIndex + 1);
+
+        return !string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(domain) && !string.IsNullOrEmpty(extension);
     }
 
     private void Login()
     {
-        string email = emailInputField.text;
-        string password = passwordInputField.text;
+        string email = emailInputField_Login.text;
+        string password = passwordInputField_Login.text;
 
         auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
         {
@@ -44,8 +150,8 @@ public class AuthManager : MonoBehaviour
 
     private void Signup()
     {
-        string email = emailInputField.text;
-        string password = passwordInputField.text;
+        string email = emailInputField_SignUp.text;
+        string password = passwordInputField_SignUp.text;
 
         auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
         {
